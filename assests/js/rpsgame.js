ddcde8fff2107b1a4f245fc3f0857cf63fa5cf6a
TheOneTheOnlyPotato <gagandhaliwal58@hotmail.com>
2024-03-18 17:07:59 -0400

    Make light dark button switch img when clicked

--- assests/js/rpsgame.js
+++ assests/js/rpsgame.js
@@ -1,5 +1,7 @@
 const modeButton = document.querySelector("#buttonLightDark");
 let main = document.querySelector("#mainSection");
+let sun = document.querySelector("#sunIMG");
+let moon = document.querySelector("#moonIMG");
 let currentMode = "light";
 
 modeButton.onclick = lightDark;
@@ -8,8 +10,12 @@ function lightDark() {
     if (currentMode == "light") {
         main.classList.add("darkMode");
         currentMode = "dark";
+        sun.classList.remove("hide")
+        moon.classList.add("hide")
     } else {
         main.classList.remove("darkMode");
         currentMode = "light";
+        sun.classList.add("hide")
+        moon.classList.remove("hide")
     }
 }
