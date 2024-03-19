7819aa142944047e50873f97724f730ba50dadba
TheOneTheOnlyPotato <gagandhaliwal58@hotmail.com>
2024-03-19 17:09:56 -0400

    Declare required js components

--- assests/js/rpsgame.js
+++ assests/js/rpsgame.js
@@ -1,8 +1,28 @@
 const modeButton = document.querySelector("#buttonLightDark");
+const rockButton = document.querySelector("#rockButton");
+const paperButton = document.querySelector("#paperButton");
+const scissorsButton = document.querySelector("#scissorButton");
+
 let main = document.querySelector("#mainSection");
 let sun = document.querySelector("#sunIMG");
 let moon = document.querySelector("#moonIMG");
+let mainGameText = document.querySelector("#mainGameText");
+let playerPointText = document.querySelector("#playerPointText");
+let computerPointText = document.querySelector("#computerPointText");
+
+const playerRock = document.querySelector("#playerGameRock"); 
+const playerPaper = document.querySelector("#playerGamePaper"); 
+const playerScissor = document.querySelector("#playerGameScissor"); 
+const computerRock = document.querySelector("#computerGameRock"); 
+const computerPaper = document.querySelector("#computerGamePaper"); 
+const computerScissor = document.querySelector("#computerGameScissor"); 
+
 let currentMode = "light";
+let playerPoints = 0;
+let computerPoints = 0;
+
+
+
 
 modeButton.onclick = lightDark;
 
