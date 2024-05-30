fee669c45a7ed478aac81b412775c5b5d32d93d2
TheOneTheOnlyPotato <gagandhaliwal58@hotmail.com>
2024-05-30 17:02:33 -0400

    Complete main palindrome js

---
+++ assests/js/palindrome.js
@@ -0,0 +1,30 @@
+let textInput = document.getElementById('textInput');
+let button = document.getElementById('checkButton');
+let result = document.getElementById('resultText');
+let userInput;
+let inputReverse;
+
+
+button.onclick = runCheck;
+
+
+function runCheck() {
+    let userInput = textInput.value;
+    inputReverse = reverseString(userInput);
+    if (userInput == inputReverse) {
+        result.innerText = `${userInput.toUpperCase()} IS a palindrome.`;
+    } else if (userInput == "") {
+        result.innerText = `Please enter a word`;
+    } else {
+        result.innerText = `${userInput.toUpperCase()} is NOT a palindrome.`;
+    }
+    result.classList.remove("hidden");
+    textInput.value = "";
+}
+
+function reverseString(userInput) {
+    let splitString = userInput.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
+}
